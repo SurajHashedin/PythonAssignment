1a6e9268731b4cd935d05a8c895ca61ed8752aa1
SurajHashedin <sukumarsoni@deloitte.com>
2022-09-13 08:59:01 +0530

    added all basic features

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+from http.client import HTTPException
 from typing import List
 from uuid import uuid4
 from fastapi import FastAPI
-from models import DataTracker
+from models import DataTracker,updateDataTracker
 
 # create an app object
 app = FastAPI()
@@ -16,10 +17,53 @@ db: List[DataTracker] = [ \
     value = 'valuetest', \
     datatype = 'datatypetest', \
     tags = 'tagstest', \
-    createdAt= datetime.now(), \
-    updatedAt= datetime.now() )]
+    createdAt = datetime.now(), \
+    updatedAt = datetime.now()
+ )]
+
 
 #fetching all metrics records 
 @app.get("/api/v1/metrics")
 async def fetch_metrics():
     return db 
+
+#Add metrics records 
+@app.post("/api/v1/metrics")
+async def create_metrics(datatracker: DataTracker):
+    datatracker.createdAt = datetime.now()
+    datatracker.updatedAt = datetime.now()
+    db.append(datatracker)
+    return {'id': datatracker.id}
+
+#delete metrics records 
+@app.delete("/api/v1/metrics/{username}")
+async def delete_username(username:str):
+    for data in db:
+        if data.username == username:
+            db.remove(data)
+            return 'deleted'
+    raise HTTPException(
+        status_code = 404,
+        detail = f"{username} is deleted"
+    )
+
+#update metrics records 
+@app.put("/api/v1/metrics/{data}")
+async def update_data(data_update: updateDataTracker, data:str):
+    for dt in db:
+        if dt.username == data:
+            if data_update.key is not None:
+                dt.key = data_update.key
+            if data_update.value is not None:
+                dt.value = data_update.value
+            if data_update.datatype is not None:
+                dt.datatype = data_update.datatype
+            if data_update.tags is not None:
+                dt.tags = data_update.tags
+
+            dt.updatedAt = datetime.now()
+            return 'Updated'
+    raise HTTPException(
+        status_code = 404,
+        detail = f"{data} is deleted"
+    )
--- app/models.py
+++ app/models.py
@@ -3,12 +3,22 @@ from pydantic import BaseModel
 from uuid import UUID, uuid4
 from typing import Optional, Union
 
+#metrics data field
 class DataTracker(BaseModel):
-    id: Optional[UUID] = uuid4
+    id: Optional[UUID]= uuid4()
     username: str 
     key: str
     value: str
     datatype: str
     tags: str 
-    createdAt: Union[datetime,None] = None
-    updatedAt: Union[datetime,None] = None 
+    createdAt: Union[datetime,None]=None
+    updatedAt: Union[datetime,None]=None 
+
+#class for update field
+class updateDataTracker(BaseModel):
+    username: str 
+    key: Optional[str]
+    value: Optional[str]
+    datatype: Optional[str]
+    tags: Optional[str]
+    updatedAt: Optional[datetime]
